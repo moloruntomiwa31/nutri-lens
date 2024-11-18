23ff160f5fbf07f06ce1b72cf14989b8f30a4851
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2024-11-18 02:44:14 +0100

    #.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,10 +32,8 @@ export default defineNuxtConfig({
       firebaseApiKey: import.meta.env.NUXT_PUBLIC_FIREBASE_API_KEY,
       firebaseAuthDomain: import.meta.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
       firebaseProjectId: import.meta.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: import.meta.env
-        .NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: import.meta.env
-        .NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseStorageBucket: import.meta.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: import.meta.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: import.meta.env.NUXT_PUBLIC_FIREBASE_APP_ID,
       apiSecret: import.meta.env.NUXT_API_SECRET,
     },
